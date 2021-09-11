8a99a5a05e0ebb76d6b83907dfed96d9708d7dd7
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-11 12:34:26 +0300

    rendering data of html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const wrapperTabsEl = document.getElementById("wrapperTabs");
 const wrapperContainerTabsEl = document.getElementById("wrapperContainerTabs");
+const wrapperUkraineEl = document.getElementById("Ukraine");
+const wrapperWorldEl = document.getElementById("World");
+const currentTime = new Date(Date.now()).toISOString().slice(0, 10);
+const yestardayTime = new Date(Date.now()-86400000).toISOString().slice(0, 10);
+let ukraineData = [];
+let worldData = [];
 
 wrapperTabsEl.addEventListener("click", (e) => {
   const tab = e.target.closest("button");
@@ -11,3 +17,39 @@ wrapperTabsEl.addEventListener("click", (e) => {
     document.getElementById(region).classList.add('active_tab');
   }
 });
+
+fetch(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
+.then(res => res.json())
+.then(data => {
+  ukraineData = data.ukraine;
+  worldData = data.world;
+  renderCoronaData(wrapperUkraineEl, ukraineData)
+  renderCoronaData(wrapperWorldEl, worldData)
+})
+.catch(error => console.warn(error));
+
+fetch(`https://api-covid19.rnbo.gov.ua/data?to=${yestardayTime}`)
+.then(res => res.json())
+.then(data => {
+  ukraineData = data.ukraine;
+  worldData = data.world;
+})
+.catch(error => console.warn(error));
+
+function renderCoronaData(elemForRender, dataArray) {
+  elemForRender.innerHTML = createDataArr(dataArray).join('');
+}
+
+function createDataArr(dataArray) {
+  return dataArray.map(field => createDataField(field));
+}
+
+function createDataField(field) {
+  return `<dl class="wrapper-data">
+            <dt class="wrapper-data__country">${field?.label?.uk}</dt>
+            <dd class="wrapper-data__confirmed">${field?.confirmed}</dd>
+            <dd class="wrapper-data__deaths">${field?.deaths}</dd>
+            <dd class="wrapper-data__recovered">${field?.recovered}</dd>
+            <dd class="wrapper-data__existing">${field?.existing}</dd>
+          </dl>`
+}
