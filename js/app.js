ec6888f838ab40bf1fd1035b80f49dd2679523b5
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-11 15:08:17 +0300

    creating general HTML

--- js/app.js
+++ js/app.js
@@ -2,54 +2,130 @@ const wrapperTabsEl = document.getElementById("wrapperTabs");
 const wrapperContainerTabsEl = document.getElementById("wrapperContainerTabs");
 const wrapperUkraineEl = document.getElementById("Ukraine");
 const wrapperWorldEl = document.getElementById("World");
+const keys = ['confirmed', 'deaths', 'recovered', 'existing']
 const currentTime = new Date(Date.now()).toISOString().slice(0, 10);
-const yestardayTime = new Date(Date.now()-86400000).toISOString().slice(0, 10);
+const yestardayTime = new Date(Date.now() - 86400000)
+  .toISOString()
+  .slice(0, 10);
 let ukraineData = [];
 let worldData = [];
+let abdd = [];
+let yestardayConfirmed = 0;
 
 wrapperTabsEl.addEventListener("click", (e) => {
   const tab = e.target.closest("button");
-  Array.from(wrapperTabsEl.children).forEach(btn => btn.classList.remove('active'));
+  Array.from(wrapperTabsEl.children).forEach((btn) =>
+    btn.classList.remove("active")
+  );
   if (tab) {
-    tab.classList.add('active');
+    tab.classList.add("active");
     const region = tab.dataset.region;
-    Array.from(wrapperContainerTabsEl.children).forEach(tabPage => tabPage.classList.remove('active_tab'));
-    document.getElementById(region).classList.add('active_tab');
+    Array.from(wrapperContainerTabsEl.children).forEach((tabPage) =>
+      tabPage.classList.remove("active_tab")
+    );
+    document.getElementById(region).classList.add("active_tab");
   }
 });
 
-fetch(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
-.then(res => res.json())
-.then(data => {
-  ukraineData = data.ukraine;
-  worldData = data.world;
-  renderCoronaData(wrapperUkraineEl, ukraineData)
-  renderCoronaData(wrapperWorldEl, worldData)
-})
-.catch(error => console.warn(error));
+/* fetch(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
+  .then((res) => res.json())
+  .then((data) => {
+    ukraineData = data.ukraine;
+    worldData = data.world;
+    // renderCoronaData(wrapperUkraineEl, ukraineData);
+    // renderCoronaData(wrapperWorldEl, worldData);
+    console.log(ukraineData);
+  })
+  .catch((error) => console.warn(error)); */
 
-fetch(`https://api-covid19.rnbo.gov.ua/data?to=${yestardayTime}`)
-.then(res => res.json())
-.then(data => {
-  ukraineData = data.ukraine;
-  worldData = data.world;
-})
-.catch(error => console.warn(error));
+function currentFetchFromDataCenter() {
+  fetch(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
+    .then((res) => res.json())
+    .then((data) => {
+      ukraineData = data.ukraine;
+      worldData = data.world;
+      yesterdayFetchFromDataCenter(`https://api-covid19.rnbo.gov.ua/data?to=${yestardayTime}`)
+    })
+    .catch((error) => console.warn(error));
+}
+
+currentFetchFromDataCenter();
+
+function yesterdayFetchFromDataCenter(url) {
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    // yestardayConfirmed = data.ukraine.map((res) => res.confirmed);
+    yestardayUkraineData = data.ukraine;
+
+
+    console.log(createSmartKeyForDifferenceCount(ukraineData, yestardayUkraineData, keys))
+
+// console.log(createDifferenceFieldsets(yestardayConfirmed));
+
+
+
+
+
+    renderCoronaData(wrapperUkraineEl, ukraineData);
+    renderCoronaData(wrapperWorldEl, worldData);
+  })
+  .catch((error) => console.warn(error));
+}
+
+/* fetch(`https://api-covid19.rnbo.gov.ua/data?to=${yestardayTime}`)
+  .then((res) => res.json())
+  .then((data) => {
+    yestardayConfirmed = data.ukraine.map((res) => res.confirmed);
+    console.log(ukraineData);
+    renderCoronaData(wrapperUkraineEl, ukraineData);
+    renderCoronaData(wrapperWorldEl, worldData);
+  })
+  .catch((error) => console.warn(error)); */
 
 function renderCoronaData(elemForRender, dataArray) {
-  elemForRender.innerHTML = createDataArr(dataArray).join('');
+  elemForRender.innerHTML = createDataArr(dataArray).join("");
 }
 
 function createDataArr(dataArray) {
-  return dataArray.map(field => createDataField(field));
+  return dataArray.map((field) => createDataField(field));
 }
 
 function createDataField(field) {
   return `<dl class="wrapper-data">
             <dt class="wrapper-data__country">${field?.label?.uk}</dt>
-            <dd class="wrapper-data__confirmed">${field?.confirmed}</dd>
+            <dd class="wrapper-data__confirmed">
+              ${field?.confirmed}
+            </dd>
             <dd class="wrapper-data__deaths">${field?.deaths}</dd>
             <dd class="wrapper-data__recovered">${field?.recovered}</dd>
             <dd class="wrapper-data__existing">${field?.existing}</dd>
-          </dl>`
+          </dl>`;
 }
+
+function createSmartKeyForDifferenceCount(dataArrayFirst, dataArraySecond, smartKeys) {
+  return smartKeys.map(smartKey => createYestardayData(dataArrayFirst, dataArraySecond, smartKey))
+}
+
+function createYestardayData(dataArrayFirst, dataArraySecond, smartKey) {
+  let arrToday = dataArrayFirst.map((res) => res[smartKey]);
+  let arrYesterday = dataArraySecond.map((res) => res[smartKey]);
+  let newArr = [];
+  for (let i = 0; i < arrToday.length; i++) {
+    newArr.push(arrToday[i] - arrYesterday[i]);
+  }
+  return createDifferenceFieldsets(newArr);
+}
+
+function createDifferenceFieldsets(fieldsets) {
+  return fieldsets.map(field => createDifferenceField(field)).join('')
+}
+
+function createDifferenceField(field) {
+  return `${field > 0 ? `<p>&#8593;${field}</p>` : `<p>&#8595;${field}</p>`}`
+}
+
+
+
+
+
